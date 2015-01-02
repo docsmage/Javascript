739f6d005dbe790de47feb3a6fb4382b333156e4
Renee CS <voiceofrae@gmail.com>
2015-01-01 16:19:28 -0800

    cleaning up files

--- mad_libs.js
+++
@@ -1,8 +0,0 @@
-var button = document.getElementById('lib-button');
-var makeMadLib = function(event) {
-      var myNoun = document.getElementById("noun").value;
-      var myAdjective = document.getElementById("adjective").value;
-      var myName = document.getElementById("person").value;
-    alert("Hi " + myName + ", " + myNoun + " is great but " + myAdjective + " is better.");
-};
-button.addEventListener('click', makeMadLib);
